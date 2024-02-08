62420edafa63a91ae75a2bc1f96e68c386be0cef
Razzi Abuissa <razzi@abuissa.net>
2024-02-08 04:31:26 -0600

    Move fish keybindings from config.fish to keybindings.fish

--- fish_conf.d/config.fish
+++ fish_conf.d/config.fish
@@ -1,34 +1,3 @@
-# Binds option-up
-bind \e\[1\;5A history-token-search-backward
-
-# Binds super-up (for emacs vterm integration, where there is no "option"
-bind \e\[1\;2A history-token-search-backward
-
-# option-down
-bind \e\[1\;5B history-token-search-forward
-
-# super-down
-bind \e\[1\;2B history-token-search-forward
-
-# Make C-t transpose characters :)
-bind \ct transpose-chars
-
-# Make C-s accept autocompletion and submit :))
-bind \cs accept-autosuggestion execute
-
-# Make C-w delete a whole word including special chars
-bind \cw backward-kill-word
-
-# Make A-bksp delete a whole word including special chars
-bind \u00FF backward-kill-bigword
-
-# Bind paste using function to avoid 'clone-cd git clone'
-bind \cv paste_avoiding_double_git_clone
-bind \ev paste_avoiding_double_git_clone
-
-# kitty keyboard protocol alt+v
-bind \u00F6 paste_avoiding_double_git_clone
-
 set -U fish_features ampersand-nobg-in-token,qmark-noglob
 
 test -e ~/.profile && source ~/.profile
