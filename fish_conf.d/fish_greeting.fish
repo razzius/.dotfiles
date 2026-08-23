6af65bf7ed48ec533f2dea93ef6a90182e13b562
Razzi Abuissa <razzi@abuissa.net>
2026-08-23 13:08:38 -0500

    Display bpm of hitting enter at prompt - could make sure the prompt is empty each time

--- fish_conf.d/fish_greeting.fish
+++ fish_conf.d/fish_greeting.fish
@@ -1,3 +1,4 @@
 function fish_greeting
+    set -U beats START
     echo '>*)))>< <*)))>< "how is your posture?"'
 end
--- fish_conf.d/fish_prompt.fish
+++ fish_conf.d/fish_prompt.fish
@@ -1,4 +1,39 @@
+function clear_bpm_on_normal_command --on-event fish_postexec
+    set -e beats
+end
+
+bind ctrl-l 'set -e beats && clear && fish_prompt'
+# bind ctrl-l 'set -e beats && clear'
+
+function display_bpm
+    if equals $beats START
+        set -e beats
+        return
+    end
+
+    set -U beats (gdate +%s.%3N) $beats[1..3]
+
+    if test (count $beats) -lt 4
+        return
+    end
+
+    set diffs \
+        (math $beats[1] - $beats[2]) \
+        (math $beats[2] - $beats[3]) \
+        (math $beats[3] - $beats[4])
+
+    set avg (math "($diffs[1] + $diffs[2] + $diffs[3]) / 3")
+    set bps (math 1 / $avg)
+    set bpm (math "$bps * 60")
+
+    if test $bpm -gt 55
+        printf "%.0f " $bpm
+    end
+end
+
 function fish_prompt --description 'Write out the prompt'
+    display_bpm
+
     set -l last_status $status
 
     if not test $PWD = $HOME
